0c15b7ee693eda8e14a2fd7c052f873a5806803c
maicobernal <bernalmaicogmail.com>
2022-07-09 15:04:39 -0300

    Upload del desafio 5, parte 1 handlebars

---
+++ desafio5/server.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const app = express()
+const {engine}= require('express-handlebars')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.engine('hbs', engine({
+    extname: 'hbs',
+    defaultLayout: 'default',
+    layoutDir: "/views/layouts",
+  }))
+ 
+app.set('view engine', 'hbs');
+app.set('views', "./views");
+
+const productos = require('./productos.js')
+
+app.use(express.static('public'));
+app.use ('/api/productos', productos)
+
+const port = 8080
+const server = app.listen(port,()=>{
+        console.log('Servidor corriendo en el puerto: ' + server.address().port)
+    })
+
+server.on('error', (error)=>{
+    console.log(`Error en el servidor ${error}`)
+})
+
